9698df7f03113b68e26a769336cf4827597fe906
rabinzon <ildarrazin@gmail.com>
2016-10-19 20:22:29 +0300

    feat(list): pass submit prop to ListItem

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,10 +4,15 @@ import ListItem from '../ListItem';
 class AddList extends React.Component {
   static propTypes = {
     lists: React.PropTypes.array,
+    submit: React.PropTypes.func,
   };
 
   renderLists = (item, key) =>
-    <ListItem item={item} key={key} />;
+    <ListItem
+      item={item}
+      key={key}
+      submit={this.props.submit}
+    />;
 
   render() {
     const { lists } = this.props;
