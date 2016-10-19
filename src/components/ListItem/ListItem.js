920e862149f8b32e8435c1664fb3d0836375c178
rabinzon <ildarrazin@gmail.com>
2016-10-19 20:23:55 +0300

    feat(ListItem): remake component

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import Card from '../Card';
 
-const renderCards = (card, key) => <Card card={card} key={key} />;
+class ListItem extends React.Component {
+  propTypes = {
+    item: React.PropTypes.object,
+    submit: React.PropTypes.func,
+  };
 
-const ListItem = ({ item }) => { // eslint-disable-line
-  return (
-    <div className='list'>
-      <div className='list__header'>
-        {item.title}
-        <div className='list__header-cards'>
-          {item.items.length} cards
+  state = {
+    val: '',
+    id: this.props.item.id,
+  };
+
+  handleNewCard = (e) => {
+    this.setState({ val: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { val, id } = this.state;
+    this.props.submit(val, id);
+  };
+
+  renderCards = (card, key) => <Card card={card} key={key} />;
+
+  render() {
+    const {
+      item,
+    } = this.props;
+
+    return (
+      <div className='list'>
+        <div className='list__header'>
+          {item.title}
+          <div className='list__header-cards'>
+            {item.items.length} cards
+          </div>
         </div>
+        {item.items.map(this.renderCards)}
+        <form className='list__add-card' onSubmit={this.handleSubmit}>
+          <input
+            className='list__add-card-input'
+            type='text'
+            placeholder='add card...'
+            onChange={this.handleNewCard}
+          />
+        </form>
       </div>
-      {item.items.map(renderCards)}
-    </div>
-  );
-};
-
-ListItem.propTypes = {
-  item: React.PropTypes.object,
-};
+    );
+  }
+}
 
 export default ListItem;
