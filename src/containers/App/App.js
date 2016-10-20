1fa3c629b743e632590e353c51686b9b4cf28b6c
rabinzon <ildarrazin@gmail.com>
2016-10-20 11:51:18 +0300

    style(App): linter

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -53,7 +53,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="wrapper">
+      <div className='wrapper'>
         <List
           lists={this.props.store.lists}
           submit={this.addCard}
